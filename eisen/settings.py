0a1e266558f0ad13583dbaa6378b0f62eee32b4c
himadrikumar <77405040+himadrikumar@users.noreply.github.com>
2024-12-08 19:45:08 +0530

    Add files via upload

--- eisen/settings.py
+++ eisen/settings.py
@@ -78,10 +78,17 @@ WSGI_APPLICATION = 'eisen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL')
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
