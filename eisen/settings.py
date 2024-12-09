edfd95423ef81437735dea17a093700ee2e8a452
himadrikumar <77405040+himadrikumar@users.noreply.github.com>
2024-12-09 13:46:02 +0530

    Add files via upload

--- eisen/settings.py
+++ eisen/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'eisen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://eisen_sql_user:73dQLbn1wNP9JLb7o1X0BR2BHV7mloAR@dpg-cta8lpqj1k6c738jdnk0-a/eisen_sql'
-    )
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL')
+#     )
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
