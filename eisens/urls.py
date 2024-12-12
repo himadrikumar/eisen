7ed1e2e51723df82e2cd3538727a6aaf1b7f9a79
himadrikumar <77405040+himadrikumar@users.noreply.github.com>
2024-12-12 17:00:09 +0530

    Update urls.py

--- eisens/urls.py
+++ eisens/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.landing_page, name='landing_page'),  # Landing page
     path('app/', views.index, name='index'),
     path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('account/', views.account_details, name='account_details'),
 ]
