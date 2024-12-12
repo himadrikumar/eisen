efc573b3d2cafca0a4319c1acb60e3c3ebc5ef64
himadrikumar <77405040+himadrikumar@users.noreply.github.com>
2024-12-12 17:51:57 +0530

    Update forms.py

--- eisens/forms.py
+++ eisens/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Task
+from django.contrib.auth.models import User
 
 class TaskForm(forms.ModelForm):
     class Meta:
@@ -8,4 +9,16 @@ class TaskForm(forms.ModelForm):
         widgets = {
             'text': forms.TextInput(attrs={'placeholder': 'Enter task here...'}),
             'category': forms.Select()
+        }
+
+
+class CustomUserChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']  # Only include safe fields
+        labels = {
+            'username': 'Username',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email Address',
         }
